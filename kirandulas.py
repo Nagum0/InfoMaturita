c311e889e09f3607e60c9cf5df362d8913edd733
Zeki Bene <zackbene9@gmail.com>
2023-04-17 21:07:06 +0200

    Completed

--- kirandulas.py
+++ kirandulas.py
@@ -1,16 +1,15 @@
 def main() -> str:
     with open("Files\\kirandulas.txt", "r") as file:
         ageList = []
-        avgAge = 0
         ageSum = 0
         numOfStudents = 0
 
         for i, j in enumerate(file):
-            numOfStudents += 1
             data = j.strip()
 
             if i % 2 != 0:
                 ageList.append(data)
+                numOfStudents += 1
 
         for i in ageList:
             ageSum += int(i)
