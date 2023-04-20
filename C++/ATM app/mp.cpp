b50c9541872e46dadfd2e925d4f3e2bb60fd1912
Zeki Bene <zackbene9@gmail.com>
2023-04-20 19:52:55 +0200

    Collected data into a map from userData.txt.

--- C++/ATM app/mp.cpp
+++ C++/ATM app/mp.cpp
@@ -1,41 +1,27 @@
 #include <iostream>
 #include <map>
-#include <vector>
 #include <fstream>
 #include <typeinfo>
 using namespace std;
 
-map<string, string> mp {
-    {"Zeki", "noooooooo"},
-    {"Astolfo", "1"},
-    {"Ferris", "0"}
-};
-
 map<string, string> map2;
 
-void editMap(map<string, string> argMap);
+void editMap(map<string, string>& argMap);
 
 int main() {
-    editMap(mp);
-    
+    editMap(map2);
+
     return 0;
 }
 
-void editMap(map<string, string> argMap) {
+void editMap(map<string, string>& argMap) {
     string fileName = "C:\\Users\\csabe\\OneDrive\\Documents\\01 MATURITA\\Info\\Tetelek\\C++\\ATM app\\userData.txt";
-    string line;
-    vector<string> text;
+    string key, value;
     ifstream inputFile(fileName);
 
-    if (!inputFile.is_open()) {
-        cout << "File: " << fileName << " didn't open.";
-    }
-
-    while (getline(inputFile, line)) {
-        text.push_back(line);
+    while (inputFile >> key >> value) {
+        argMap[key] = value;
     }
 
     inputFile.close();
-
-    cout << typeid(text[0]).name() << "\n";
 }
