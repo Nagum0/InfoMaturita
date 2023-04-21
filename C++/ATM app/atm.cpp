ceeeff350561bad721e0a4babffc9404e1dec674
Zeki Bene <zackbene9@gmail.com>
2023-04-21 14:59:22 +0200

    Readfile function added.

--- C++/ATM app/atm.cpp
+++ C++/ATM app/atm.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <map>
+#include <fstream>
 using namespace std;
 
 /* Info structure */
@@ -8,20 +10,28 @@ struct Info {
     int age;
     int balance;
     string pin;
-    bool newAccount = true; //0 = true, 1 = false
+    bool newAccount; //0 = true, 1 = false
 };
 
 /* Variables */
 Info user;
+map<string, string> dataMap;
 
 /* Function declarations */
 void personSetup(Info &user);
 void personInfo(Info &person);
 void listOfCmds();
+void readFile(map<string, string>& argMap);
 
 /* Main function */
 int main() {
     cout << "------------Zeki's Bank ATM------------" << "\n";
+    //Reading data file
+    readFile(dataMap);
+
+    for (auto itr = dataMap.begin(); itr != dataMap.end(); itr++) {
+        cout << (*itr).first << " : " << (*itr).second << "\n";
+    }
 
     //Command variable
     string cmd;
@@ -61,4 +71,21 @@ void personInfo(Info &person) {
 //Command list
 void listOfCmds() {
     cout << "Command list: \n";
+}
+
+//Read text file
+void readFile(map<string, string>& argMap) {
+    string fileName = "C:\\Users\\csabe\\OneDrive\\Documents\\01 MATURITA\\Info\\Tetelek\\C++\\ATM app\\userData.txt";
+    string key, value;
+    ifstream file(fileName);
+
+    if (!file) {
+        cerr << "Failed to open file " << fileName << "\n";
+    }
+
+    while (file >> key >> value) {
+        argMap.insert(pair<string, string>(key, value));
+    }
+
+    file.close();
 }
