caec5aa7078c17ca9fb4d8c87d7acc59a10b1c13
Zeki Bene <zackbene9@gmail.com>
2023-04-22 10:50:04 +0200

    Reverted

---
+++ C++/Documentation/doc_app/src/chapters/Home.jsx
@@ -0,0 +1,11 @@
+import "../App.css";
+
+function Home() {
+    return (
+        <div>
+            Home
+        </div>
+    );
+}
+
+export default Home;
--- C++/Documentation/doc_app/src/components/Menu.jsx
+++ C++/Documentation/doc_app/src/components/Menu.jsx
@@ -1,5 +1,5 @@
 import '../App.css';
-import { useState } from 'react';
+import { useState } from 'react';;
 
 function Menu () {
     /* For tetelek dropdown menu */
@@ -10,17 +10,22 @@ function Menu () {
     const [projectsOpen, setProjectsOpen] = useState(false);
     const toggleProjectsOpen = ()=> { setProjectsOpen(!projectsOpen); }
 
+    /* About me dropdown menu */
+    const [aboutMeOpen, setAboutMeOpen] = useState(false);
+    const toggleAboutMe = ()=> { setAboutMeOpen(!aboutMeOpen); }
+
     return (
         <div className='menu_div'>
             {/* Title */}
-            <h1 className='menu_title'>
-                Menu
-            </h1>
+                <h1 className='menu_title'>
+                    Menu
+                </h1>
 
             {/* Tetelek dropdown menu */}
             <a href="#" className='drop_down_link' onClick={toggleMenu}>
                 Tetelek
             </a>
+
             {isOpen && (
                 <ul className='drop_down_menu_ul'>
                     <li className="drop_down_item">
@@ -39,6 +44,30 @@ function Menu () {
             <a href="#" className='drop_down_link' onClick={toggleProjectsOpen}>
                 C++ Projects
             </a>
+
+            {projectsOpen && (
+                <ul className="drop_down_menu_ul">
+                    <li className="drop_down_item">
+                        ATM Terminal App
+                    </li>
+                </ul>
+            )}
+
+            {/* About me */}
+            <a href="#" className='drop_down_link' onClick={toggleAboutMe}>
+                About me
+            </a>
+
+            {aboutMeOpen && (
+                <ul className="drop_down_menu_ul">
+                    <li className="drop_down_item">
+                        Instagram
+                    </li>
+                    <li className="drop_down_item">
+                        Twitter
+                    </li>
+                </ul>
+            )}
         </div>
     );
 }
